89476c5b73ff31ccadcae5fa77d942254e5e488b
milad-sedhom <miladsedhom@gmail.com>
2024-03-17 00:57:44 +0200

    style: global style and some basic styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { page } from '$app/stores'
+</script>
+
+<header>
+	<a href="/"> Science talk </a>
+	{#if $page.data.user}
+		<p>Hi {$page.data.user.username}</p>
+	{/if}
+
+	<nav>
+		{#if $page.data.user}
+			<li>
+				<a href="/logout">logout</a>
+			</li>
+		{:else}
+			<ul>
+				<li>
+					<a href="/login">login</a>
+				</li>
+				<li>
+					<a href="/register">register</a>
+				</li>
+			</ul>
+		{/if}
+	</nav>
+</header>
+<h1>Welcome to Science Talk</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia inventore dolor</p>
+
+<pre>{JSON.stringify($page, null, 2)}</pre>
+
+<style>
+	:global(body) {
+		background-color: rgb(47, 49, 50);
+		color: white;
+		font-family: sans-serif;
+
+		& a {
+			text-decoration: none;
+
+			&:visited {
+				color: white;
+			}
+			&:hover {
+				color: dodgerblue;
+				text-decoration: underline;
+			}
+		}
+	}
+</style>
